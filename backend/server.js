4c8bda94eb5e86d6381164bc33c82d8fab702919
Jay Joshi <joshijayy421@gmail.com>
2025-11-20 05:55:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'https://authentication-app-mern.vercel.app/',
     credentials:true
 }))
 
